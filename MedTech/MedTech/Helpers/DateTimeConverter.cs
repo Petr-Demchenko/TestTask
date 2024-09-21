ca658132a740d97e97d775fe29e96f35a6810a0d
pdemchenko <pdemchenko@test.com>
2024-09-21 14:54:06 +0300

    Добавлен конвертер для отображения UTC и местного времени

---
+++ MedTech/MedTech/Helpers/DateTimeConverter.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace MedTech.Helpers
+{
+    internal class DateTimeConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values.Length == 2)
+            {
+                if (values[0] is DateTime dateTime && values[1] is bool isChecked)
+                {
+                    return (isChecked ? dateTime : dateTime.ToLocalTime()).ToString();
+                }
+            }
+
+            return Binding.DoNothing;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
